93001121a9cc7608ff1ca662a6c2520ec5983aaa
Viorel Spinu <viorel.spinu@gmail.com>
2018-05-18 20:35:24 +0300

    bug fix

--- client/src/app/component/pilot/pilot.component.ts
+++ client/src/app/component/pilot/pilot.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { Router } from '@angular/router';
 
 import { PilotService } from '../../service/pilot.service';
-import { RaceService } from '../../service/race.service';
 import { TournamentService } from '../../service/tournament.service';
 import { Pilot } from '../../model/pilot';
 import { PilotRaceEvent } from '../../model/pilot-race-event';
 import { PilotRaceTotal } from '../../model/pilot-race-total';
+import { PilotTournamentResults } from '../../model/pilot-tournament-results';
+
 
 @Component({
   selector: 'app-pilot',
@@ -17,65 +17,35 @@ import { PilotRaceTotal } from '../../model/pilot-race-total';
 })
 export class PilotComponent implements OnInit {
   pilot: Pilot;
-  rawPilotRaceEvents: PilotRaceEvent[];
-
-  results: PilotRaceEvent[][];
-  resultsTotal: PilotRaceTotal[];
-
-  bestRaceIndex: number;
-  bestRacePoints: number;
+  pilotTournamentResult: PilotTournamentResults;
 
   constructor(
     public route: ActivatedRoute,
     public pilotService: PilotService,
-    public raceService: RaceService,
     public tournamentService: TournamentService,
-    public location: Location,
-    public router: Router
-  ) {}
+    public location: Location
+  ) { }
 
   ngOnInit() {
     this.loadData();
   }
 
-  loadData():void{
+  loadData(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.pilotService.getPilotById(id).subscribe(pilot => {
       this.pilot = pilot;
       this.tournamentService
         .getPilotTournamentEvents(this.pilot.id)
         .subscribe(results => {
-          this.rawPilotRaceEvents = results.raceEvents;
-          this.resultsTotal = results.raceTotals;
-          this.bestRaceIndex = results.bestRaceIndex;
-          this.bestRacePoints = results.bestRacePoints;
-
-          this.computePilotResultsForDisplay();
+          this.pilotTournamentResult = results;
         });
     });
   }
 
-  computePilotResultsForDisplay(): void {
-    if (!this.rawPilotRaceEvents) {
-      return;
-    }
-    if (this.rawPilotRaceEvents.length === 0) {
-      return;
-    }
-
-    this.results = [];
-
-    let i = 0;
-    let raceIndex = this.rawPilotRaceEvents[0].raceIndex;
-    this.results[0] = [];
-
-    for (const event of this.rawPilotRaceEvents) {
-      if (!(event.raceIndex === raceIndex)) {
-        raceIndex = event.raceIndex;
-        i++;
-        this.results[i] = [];
-      }
-      this.results[i].push(event);
-    }
+  filterEvents(raceIndex: number): PilotRaceEvent[] {
+    return this.pilotTournamentResult.raceEvents.filter
+      (
+      event => { return event.raceIndex == raceIndex }
+      );
   }
 }
